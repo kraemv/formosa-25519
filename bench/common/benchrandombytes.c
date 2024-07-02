ed023d79908d41de232072318cd939386f1b2a25
Tiago Oliveira <tiago.oliveira@sandboxquantum.com>
2024-07-02 15:10:24 +0100

    add bench/: from Libjade; with revision to building system

---
+++ bench/common/benchrandombytes.c
@@ -0,0 +1,17 @@
+#ifndef BENCHRANDOMBYTES_C
+#define BENCHRANDOMBYTES_C
+
+#include <inttypes.h>
+#include <stdlib.h>
+
+#include "randombytes.h"
+
+extern uint8_t* __jasmin_syscall_randombytes__(uint8_t* x, uint64_t xlen);
+
+static uint8_t* benchrandombytes(uint8_t* x, uint64_t xlen)
+{
+  x = __jasmin_syscall_randombytes__(x, xlen);
+  return x;
+}
+
+#endif
