459ea51635e4c9c9a8cbe6ac3616d252385b984a
Karolin Varner <karo@cupdev.net>
2024-08-07 17:05:33 +0200

    feat(extract-libjade.sh): Refactor extract.sh code to use main function and set -e

--- extract-libjade.sh
+++ extract-libjade.sh
@@ -5,92 +5,96 @@
 # ./extract-libjade.sh --list-implementations | while read implementation; do ./extract-libjade.sh --gen-implementation $implementation ../libjade/src/$implementation; done
 #
 
-call=$0
-top_dir=$(cd "$(dirname "$0")" ; pwd -P) || exit 1
-
-print_usage()
-{
+print_usage() {
   echo "usage:"
   echo " $ $call --list-implementations"
   echo " $ $call --gen-implementation IMPLEMENTATION DIRECTORY"
 }
 
-# if there are no arguments, print usage
-if [ $# -eq 0 ]; then
-  print_usage
-  exit 1
-fi
+main() {
+  call=$0
+  top_dir=$(cd "$(dirname "$0")" ; pwd -P) || exit 1
 
-# check if --list-implementations
-if [ "$1" == "--list-implementations" ]; then
-  make --no-print-directory -C "$top_dir"/src print-available-implementations
-  exit 1
-fi
-
-# check if --gen-implementation
-if [ "$1" == "--gen-implementation" ]; then
- # if we are generating an implementation then there should be 3 args
- if [ $# -eq 3 ]; then
-
-   # start by realpath them (useful to run make)
-   implementation="$top_dir"/src/$2
-   directory=$3
-
-   # test if IMPLEMENTATION directory exists
-   if [ ! -d "$implementation" ]; then
-     echo " IMPLEMENTATION: $implementation does not exist."
-     exit 1;
-   fi
-
-  # test if libjade DIRECTORY exists
-  if [ ! -d "$directory" ]; then
-     echo " DIRECTORY: $directory does not exist."
-     exit 1;
+  # if there are no arguments, print usage
+  if [ $# -eq 0 ]; then
+    print_usage
+    exit 1
   fi
 
-  relative_implementation=$(realpath --relative-to="$top_dir/src" "$implementation")
-  make --no-print-directory -C "$top_dir"/src/ "$relative_implementation"/preprocess-inplace
+  # check if --list-implementations
+  if [ "$1" == "--list-implementations" ]; then
+    make --no-print-directory -C "$top_dir"/src print-available-implementations
+    exit 1
+  fi
 
-  #############################################################################
-  # copy the preprocessed files
+  # check if --gen-implementation
+  if [ "$1" == "--gen-implementation" ]; then
+   # if we are generating an implementation then there should be 3 args
+   if [ $# -eq 3 ]; then
 
-  jazz_files=$(find "$implementation" -name '*.jazz')
-  for file in $jazz_files; do
-    cp "$file" "$directory"/
-  done
+     # start by realpath them (useful to run make)
+     implementation="$top_dir"/src/$2
+     directory=$3
 
-  # setup the Makefile
-  echo -n "SRCS := " > "$directory"/Makefile
-  for file in $jazz_files; do
-    echo -n "$(basename "$file")" >> "$directory"/Makefile
-  done
-  echo "" >> "$directory"/Makefile
+     # test if IMPLEMENTATION directory exists
+     if [ ! -d "$implementation" ]; then
+       echo " IMPLEMENTATION: $implementation does not exist."
+       exit 1;
+     fi
 
-  # NOTE: the following line will need change (or be deleted) once multi-repo libjade is stable
-  echo "include ../../../../Makefile.common" >> "$directory"/Makefile
+    # test if libjade DIRECTORY exists
+    if [ ! -d "$directory" ]; then
+       echo " DIRECTORY: $directory does not exist."
+       exit 1;
+    fi
 
-  # NOTE: the following command will change once there is a PR in libjade to move api.h files out of include/ directories
-  cp "$implementation"/include/api.h "$directory"/include/api.h
+    relative_implementation=$(realpath --relative-to="$top_dir/src" "$implementation")
+    make --no-print-directory -C "$top_dir"/src/ "$relative_implementation"/preprocess-inplace
 
-  #
-  #############################################################################
+    #############################################################################
+    # copy the preprocessed files
 
-  # restore implementation state
-  make --no-print-directory -C "$top_dir"/src/ "$relative_implementation"/revert-preprocess-inplace
+    jazz_files=$(find "$implementation" -name '*.jazz')
+    for file in $jazz_files; do
+      cp "$file" "$directory"/
+    done
 
-  exit 1;
+    # setup the Makefile
+    echo -n "SRCS := " > "$directory"/Makefile
+    for file in $jazz_files; do
+      echo -n "$(basename "$file")" >> "$directory"/Makefile
+    done
+    echo "" >> "$directory"/Makefile
 
- else
-  echo "error: --gen-implementation : number of required arguments 3 : provided $#"
+    # NOTE: the following line will need change (or be deleted) once multi-repo libjade is stable
+    echo "include ../../../../Makefile.common" >> "$directory"/Makefile
+
+    # NOTE: the following command will change once there is a PR in libjade to move api.h files out of include/ directories
+    cp "$implementation"/include/api.h "$directory"/include/api.h
+
+    #
+    #############################################################################
+
+    # restore implementation state
+    make --no-print-directory -C "$top_dir"/src/ "$relative_implementation"/revert-preprocess-inplace
+
+    exit 1;
+
+   else
+    echo "error: --gen-implementation : number of required arguments 3 : provided $#"
+    print_usage
+    exit 1
+   fi
+  fi
+
+  # with 'good' options this should be unreachable, hence, print usage
   print_usage
   exit 1
- fi
-fi
+}
 
-# with 'good' options this should be unreachable, hence, print usage
-print_usage
-exit 1
+init() {
+  set -e
+  main "$@"
+}
 
-
-
-
+init "$@"
